2050e76953e793b064d4ad62d7a65cae8e67bf2b
Caleb9083 <=calebosam12@gmail.com>
2022-08-19 14:56:48 +0000

    Navbar component created with routes in App.js configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./App.css";
-import User from "./components/User";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUP";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <User />
+      <BrowserRouter>
+        <div className="pages">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
