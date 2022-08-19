80ea8e73c60ef11ece26fd0a8b30c908ebc09ca6
Caleb9083 <=calebosam12@gmail.com>
2022-08-19 14:08:03 +0000

    Sign Up page added

---
+++ src/pages/SignUP.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const SignUP = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <div>
+      <form className="signup" onSubmit={onSubmitHandler}>
+        <h3>Sign up </h3>
+        <label>Email:</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUP;
