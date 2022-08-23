ff395eaeea3ff8a8b4c774e126b0244fd66ce3a0
Caleb9083 <=calebosam12@gmail.com>
2022-08-23 01:20:00 +0000

    Removed SignUp route to redeploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
+//import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 
 function App() {
@@ -15,7 +15,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
+            {
+              //<Route path="/signup" element={<SignUp />} />
+            }
           </Routes>
         </div>
       </BrowserRouter>
